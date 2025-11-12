d78014fd6fa21c26af662a11a7118e24f7ccfaa8
Omeje Remmy <remmy.ro@gmail.com>
2025-11-12 15:29:06 +0100

    feat: gif bg

--- src/components/hero.rs
+++ src/components/hero.rs
@@ -4,14 +4,15 @@ use yew::{function_component, html, Html};
 pub fn Hero() -> Html {
     html! {
         <section
-            class="min-h-screen flex justify-center items-center bg-black text-green-400 bg-cover bg-center bg-fixed"
+            style="background: url('/rcodr/static/bg.gif') center/cover fixed no-repeat;"
+            class="min-h-screen flex justify-center items-center text-green-400"
         >
             <div
-                class="max-w-4xl w-full sm:h-[400px] p-8 border border-green-400 rounded-xl bg-black/80 backdrop-blur-sm shadow-[0_0_10px_rgba(0,255,0,0.2)] font-mono flex flex-col sm:flex-row items-center sm:items-center justify-between gap-6"
+                class="max-w-4xl w-full sm:h-[200px] p-8 border border-[#262626] rounded-xl bg-[#0a0a0a] backdrop-blur-sm shadow-[0_0_10px_rgba(0,255,0,0.2)] font-mono flex flex-col sm:flex-row items-center sm:items-center justify-between gap-6"
             >
                 <div class="flex flex-col gap-4">
-                    <h1 class="text-3xl sm:text-4xl tracking-wider">
-                        <span class="text-red-700">{ "RCODR" }</span>
+                    <h1 class="text-3xl sm:text-4xl font-extrabold font-mono tracking-wider">
+                        <span class="text-red-700">{ "R C O D R" }</span>
                         <span class="ml-1 animate-blink">{"_"}</span>
                     </h1>
                     <div class="text-sm text-gray-500 animate-pulse">
@@ -23,7 +24,7 @@ pub fn Hero() -> Html {
                     <img
                         src="/rcodr/static/avatar.png"
                         alt="Omeje Remmy"
-                        class="w-48 h-38 sm:w-60 sm:h-40 rounded-full border-2 border-green-400 shadow-md"
+                        class="w-48 h-38 sm:w-60 sm:h-38 rounded-full border-2 border-[#737373] shadow-md"
                     />
                 </div>
             </div>
--- src/components/nav_bar.rs
+++ src/components/nav_bar.rs
@@ -3,7 +3,7 @@ use yew::{function_component, html, Html};
 #[function_component]
 pub fn NavBar() -> Html {
     html! {
-        <nav class="fixed top-0 w-full bg-black backdrop-blur-sm z-50">
+        <nav class="fixed top-0 w-full bg-[#0a0a0a]  backdrop-blur-sm z-50">
             <div class="max-w-6xl mx-auto px-4 py-3 flex justify-center gap-10 font-mono text-gray-400 tracking-widest">
                 <a href="https://github.com/Remmy-coder" class="hover:text-emerald-400 transition-colors">{ "github" }</a>
                 <a href="mailto:remmy.ro@gmail.com" class="hover:text-emerald-400 transition-colors">{ "mail" }</a>
@@ -13,4 +13,3 @@ pub fn NavBar() -> Html {
         </nav>
     }
 }
-
